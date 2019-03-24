beba0c49fd7553c1650cd22a0f424b03ad46d2e5
Michael Kalygin <michael.kalygin@gmail.com>
2019-03-25 07:46:35 +0800

    Add night theme support

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,15 @@ import Chart from './chart';
 
 document.addEventListener('DOMContentLoaded', () => {
   const $container = document.querySelector('main');
+  const $footer = document.querySelector('footer');
 
   window.data.forEach((data, index) => {
     const chart = new Chart(`Chart ${index + 1}`, data);
     chart.appendTo($container);
+  });
+
+  $footer.addEventListener('click', () => {
+    document.body.classList.toggle('day');
+    document.body.classList.toggle('night');
   });
 });
